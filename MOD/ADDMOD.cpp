6a1f73731695f18727478faa98cb8f5fdab71630
lovetonight <hoandao312@Gmail.com>
2022-09-02 14:01:14 +0700

    MOD

---
+++ MOD/ADDMOD.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+
+int main()
+{
+    unsigned long long a, b;
+    cin >> a;
+    cin >> b;
+    cout << a % MOD + b % MOD;
+}
