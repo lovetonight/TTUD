3f000091447cd8e2ab9db59adc11c1fd1de5d7f3
lovetonight <hoandao312@Gmail.com>
2022-09-06 17:45:28 +0700

    EXPMOD

--- MOD/EXPMOD.cpp
+++ MOD/EXPMOD.cpp
@@ -2,3 +2,24 @@
 using namespace std;
 #define MOD 1000000007
 
+unsigned long long sqr(unsigned long long x)
+{
+    return x * x;
+}
+
+unsigned long long pow(unsigned long long x, unsigned long long y)
+{
+    if (y == 0)
+        return 1 % MOD;
+    if (y % 2 == 0)
+        return sqr(pow(x, y / 2)) % MOD;
+    else
+        return ((x % MOD) * (sqr(pow(x, y / 2)) % MOD)) % MOD;
+}
+
+int main()
+{
+    unsigned long long x, y;
+    cin >> x >> y;
+    cout << pow(x, y);
+}
