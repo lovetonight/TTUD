121892ad29a56a889a6fd363e3b0d3e3766cb627
lovetonight <hoandao312@Gmail.com>
2022-09-07 00:40:44 +0700

    SUMSEQ

--- MOD/SUMSEQ.cpp
+++ MOD/SUMSEQ.cpp
@@ -1,4 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define MOD 1000000007
+#define MAX 1000000
+int arr[MAX], n;
 
+int main()
+{
+    long long sum = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+    cout << sum % MOD;
+}
