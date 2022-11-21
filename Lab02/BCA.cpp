f6143a6c7bb56f60f35ef54d92258c8d8a804f5d
lovetonight <hoandao312@Gmail.com>
2022-11-21 16:21:51 +0700

    Lab02

--- Lab02/BCA.cpp
+++ Lab02/BCA.cpp
@@ -5,61 +5,79 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int nMax = 11, mMax = 31;
-int n, m, res = mMax;
-int arr[nMax][mMax];
-int M[mMax][mMax];
-int load[nMax]; // Luu so mon hoc cua giao vien i
-int x[mMax];    // Luu giao vien day mon i
+const int nMax = 31, mMax = 11, N = 50;
+vector<int> T[N];
+int n, m, res = 1e9;
+int x[nMax];    // Giáo viên dạy môn thứ i
+int load[mMax]; // Tải của giáo viên thứ i
+bool conflict[nMax][nMax];
 
 void input()
 {
-    int k, x, y, z;
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++)
+    cin >> m >> n;
+    for (int t = 1; t <= m; t++)
     {
-        cin >> k;
-        for (int j = 1; j <= k; j++)
+        int x;
+        cin >> x;
+        for (int i = 1; i <= x; i++)
         {
-            cin >> x;
-            arr[i][x] = 1;
+            int y;
+            cin >> y;
+            T[y].push_back(t);
         }
     }
-    cin >> x;
-    for (int i = 1; i <= x; i++)
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            conflict[i][j] = false;
+    int K;
+    cin >> K;
+    for (int i = 1; i <= K; i++)
     {
-        cin >> y >> z;
-        M[y][z] = 1;
-        M[z][y] = 1;
+        int x, y;
+        cin >> x >> y;
+        conflict[x][y] = true;
+        conflict[y][x] = true;
     }
 }
+bool check(int t, int k)
+{
+    for (int i = 1; i < k; i++)
+        if (conflict[i][k] && x[i] == t)
+            return false;
+    return true;
+}
+
 void solution()
 {
-
+    int maxLoad = 0;
+    for (int i = 1; i <= m; i++)
+        maxLoad = max(maxLoad, load[i]);
+    res = min(res, maxLoad);
 }
-
-void Try(int y)
+void Try(int k)
 {
-    for (int i = 1; i <=n; i++)
+    for (int i = 0; i < T[k].size(); i++)
     {
-        if (arr[i][y] == 1)
+        int t = T[k][i];
+        if (check(t, k))
         {
-            x[y] = i;
-            load[i]++;
-            if (load[i] < res)
-                Try(y + 1);
+            x[k] = t;
+            load[t]++;
+            if (k == n)
+                solution();
+            else if (load[t] < res)
+                Try(k + 1);
+            load[t]--;
         }
-        if (y == m) solution();
-    else
-    {
-        if(load[])
     }
-    }
-    
 }
+
 int main()
 {
     input();
+    for (int i = 1; i <= m; i++)
+        load[i] = 0;
     Try(1);
     cout << res;
+    return 0;
 }
